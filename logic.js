1e0b23156dc420b652b215080e174b62326d875b
Mohammad Mahdi Bahrami <90021059+gikdev@users.noreply.github.com>
2023-12-31 17:02:30 +0330

    Add files via upload

---
+++ logic.js
@@ -0,0 +1,29 @@
+const $ = e => document.querySelector(e)
+
+const speaker = $('img')
+const changeMusicBtn = $('#change-music-btn')
+const audio = $('#audio')
+
+speaker.addEventListener('click', e => {
+  audio.paused ? audio.play() : audio.pause();
+  speaker.classList.toggle('stopped')
+})
+
+changeMusicBtn.addEventListener('click', e => {
+  audio.pause()
+  audio.setAttribute('src', chooseRandomAudio())
+  audio.play()
+})
+
+
+function chooseRandomAudio() {
+  const audios = [
+    'https://mir1.kashoob.com/audio/202212/enc_16704920669072276211589.mp3',
+    'https://mir1.kashoob.com/audio/202312/enc_17024950508165142833009.mp3',
+    'https://mir1.kashoob.com/audio/202311/enc_17009347358486038235115.mp3',
+    'https://mir1.kashoob.com/audio/202312/enc_17024317206652988061556.mp3',
+    'https://mir1.kashoob.com/audio/202312/enc_17025153673524976168630.mp3',
+  ]
+  const randomIndex = Math.floor(Math.random() * audios.length)
+  return audios[randomIndex]
+}
